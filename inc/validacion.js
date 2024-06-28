6f2a6a79403aec26fcf3ffeb45bf9104feebfb06
luisgg121 <luisgg12_1@hotmail.com>
2024-06-28 14:54:00 -0600

    Comit del 28 de junio 2024 15 hrs.

--- inc/validacion.js
+++ inc/validacion.js
@@ -1,31 +1,81 @@
 alert("Ya cargué validacion.js");
 
 const btnEnviar = document.getElementById("btnEnviar");
-alert(btnEnviar.value);
+// alert(btnEnviar.value);
 
 const validarFormulario = (e) => {
     e.preventDefault();
 
     var autor = document.forms["formulario"]["autor"].value;
     var email = document.forms["formulario"]["email"].value;
+    var telefono = document.forms["formulario"]["telefono"].value;
+    var libro = document.forms["formulario"]["libro"].value;
+    var fecha = document.forms["formulario"]["fecha"].value;
+    var resumen = document.forms["formulario"]["resumen"].value;
+    var url = document.forms["formulario"]["url"].value;
 
-    if (autor === "") {
-        alert("Por favor ingresa el nombre del autor.");
-        return false;
+    alert("ya leí las variables del formulario");
+
+    var error = false;
+    var expRegular = "";
+
+    expRegular = /^\s+$/
+    // alert(expRegular);
+    if (autor === null || autor.length === 0 || !expRegular.test(autor)) {
+        document.getElementById('autorErr').textContent = 'Por favor ingresa el nombre del autor.';
+        error = true;
+    } else {
+        document.getElementById('autorErr').textContent = "";
+
     }
 
-    const patronEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    if (patronEmail.test(email)) 
-        document.getElementById('emailErr').textContent = "";   
+    expRegular = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
+    if (expRegular.test(email))
+        document.getElementById('emailErr').textContent = "";
     else {
         document.getElementById('emailErr').textContent = 'Por favor ingresa un email válido.';
-        return false;
+        error = true;
+    }
+
+    // Validamos el número de teléfono
+    expRegular = /^[0-9]*$/;
+    if (telefono === null || telefono.length != 10 || !expRegular.test(telefono)) {
+        document.getElementById('telefonoErr').textContent = 'Por favor ingresa un número telfónico válido de 10 dígitos.';
+        error = true;
+    } else {
+        document.getElementById('telefonoErr').textContent = "";
+    }
+
+    // Validando el nombre del libro 
+    expRegular = /^[a-zA-ZÀ-ž0-9_\sñáéíóúÁÉÍÓÚ]*$/;
+    if (libro === null || libro.length == 0 || !expRegular.test(libro)) {
+        document.getElementById('libroErr').textContent = 'Por favor ingresa el nombre del libro.';
+        error = true;
+    } else {
+        document.getElementById('libroErr').textContent = "";
+    }
+
+    // Validando la fecha de publicación 
+    expRegular = /^\d{2}\/\d{2}\/\d{4}$/;
+    if (fecha === null || fecha.length == 0 || !expRegular.test(fecha)) {
+        document.getElementById('fechaErr').textContent = 'Por favor ingresa la fecha de publicación del libro.';
+        error = true;
+    }
+    else {
+        document.getElementById('fechaErr').textContent = "";
     }
 
 
+    // Si todas las comprobaciones son correctas, se presenta el reporte de campos validados
+    if (!error) {
 
-    // Si todas las comprobaciones son correctas, retorna true
-    return true;
+
+    }
+    else {
+
+
+    }
+
 }
 
 
@@ -34,25 +84,4 @@ btnEnviar.addEventListener("click", validarFormulario);
 alert("Exito en creación de btnEnviar.addEventListener");
 
 
-function validarAutor() {
-    var valor = document.getElementById("autor").value;
 
-    if (valor === null || valor.length === 0 || /^\s+$/.test(valor)) {
-        alert("Falta llenar el campo de autor.");
-        return false;
-    } else {
-        return true;
-    }
-}
-
-function validarEmail(email) {
-    const patronEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    return patronEmail.test(email);
-}
-
-const emailInput = document.getElementById('email');
-const emailValido = validarEmail(emailInput.value);
-if (!emailValido) {
-    alert("Por favor, ingresa una dirección de correo electrónico válida.");
-    emailInput.focus();
-}
